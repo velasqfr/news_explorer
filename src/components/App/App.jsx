a1abd23de7f5694beee40fd905ab254009c763d7
Franklin Velasquez <velasqfr96@gmail.com>
2025-10-17 11:00:06 -0400

    Updated index.css, main.jsx, and fonts component - along with react-dom vite

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../Header/Header";
+import About from "../About/About";
+import Footer from "../Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

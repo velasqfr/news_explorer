94c7f6d28d5f15b24ee0f96fd178700912cda4de
Franklin Velasquez <velasqfr96@gmail.com>
2025-10-20 20:37:02 -0400

    Incorporated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,35 @@
 import "./Footer.css";
+import githubIcon from "../../images/github.svg";
+import linkedIcon from "../../images/linkedIn.svg";
 
 function Footer() {
   return (
     <footer className="footer">
-      <p> @2025 Supersite, Powered by News API</p>
-      <nav>
-        <a>Home</a>
-        <a>Tripleten</a>
-      </nav>
-      <div className="social-icons">GH,LI</div>
+      <div className="footer__container">
+        <p className="footer__copyright">
+          &copy; 2025 Supersite, Powered by News API
+        </p>
+        <nav className="footer__nav">
+          <a href="/">Home</a>
+          <a href="/tripleten">Tripleten</a>
+        </nav>
+        <div className="footer__social-icons">
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubIcon} alt="GitHub" className="footer__icon" />
+          </a>
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedIcon} alt="LinkedIn" className="footer__icon" />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }

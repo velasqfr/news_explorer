16c7ce9be8ef354c13b11ec487c681cc7bf51f0c
Franklin Velasquez <velasqfr96@gmail.com>
2025-11-17 11:19:32 -0500

    Added hamburger menu and adjusting phone screen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import logout from "../../images/logoutw.svg";
-// import menuOpen from "../../images/menuHomeIcon.svg";
-// import menuClose from "../../images/menuSaveIcon.svg";
+import menuOpenIcon from "../../images/menuHomeIcon.svg";
+import closeIcon from "../../images/close.svg";
+// import menuCloseIcon from "../../images/menuSaveIcon.svg";
 
 function Header({
   isLoggedIn,
@@ -14,9 +15,34 @@ function Header({
 }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  // Toggle mobile menu visibility
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+  const closeMenu = (event) => {
+    if (event.target.closest(".header__menu") === null) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.addEventListener("click", closeMenu);
+    } else {
+      document.removeEventListener("click", closeMenu);
+    }
+
+    return () => {
+      document.removeEventListener("click", closeMenu);
+    };
+  }, [isMenuOpen]);
+
   return (
     <header className={`header ${isLoggedIn ? "header__logged-in" : ""}`}>
-      <div className="header__container">
+      <div
+        className={`header__container ${isMenuOpen ? "header__container--open" : ""}`}
+      >
         <h1 className="header__logo">NewsExplorer</h1>
 
         <div className="header__menu">
@@ -34,6 +60,14 @@ function Header({
               </Link>
             )}
           </nav>
+          {/* Hamburger */}
+          <button
+            className="header__hamburger"
+            onClick={toggleMenu}
+            aria-label="Toggle menu"
+          >
+            <img src={isMenuOpen ? closeIcon : menuOpenIcon} alt="Menu" />
+          </button>
 
           {isLoggedIn ? (
             <button className="header__sign-out" onClick={onSignOutClick}>
@@ -41,11 +75,37 @@ function Header({
               <img src={logout} alt="Log out" className="signout__icon" />
             </button>
           ) : (
+            /* Sign In */
             <button className="header__sign-in" onClick={onSignInClick}>
               Sign In
             </button>
           )}
         </div>
+
+        {isMenuOpen && (
+          <div className="header__drawer">
+            <nav className="header__nav-drawer">
+              <Link to="/" className={!isSavedNewsPage ? "active" : ""}>
+                Home
+              </Link>
+
+              {isLoggedIn && (
+                <Link
+                  to="/saved-news"
+                  className={isSavedNewsPage ? "active" : ""}
+                >
+                  Saved Articles
+                </Link>
+              )}
+
+              {!isLoggedIn && (
+                <button className="drawer__signin-btn" onClick={onSignInClick}>
+                  Sign In
+                </button>
+              )}
+            </nav>
+          </div>
+        )}
       </div>
     </header>
   );
